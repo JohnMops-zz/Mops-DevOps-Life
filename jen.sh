813432db64772c686d0ac3578fe7df81453469a2
JohnMops <Evgenibir@outlook.com>
2019-06-30 07:47:46 +0300

    Update jen.sh

--- jen.sh
+++ jen.sh
@@ -1,2 +1,18 @@
-#!/bin/bash
-docker run --name web nginx
+pipeline{
+    agent {
+        label 'master'
+    }
+    stages {
+        stage('git access'){
+            steps{
+                git credentialsId: 'e7495d2e-d594-4038-8f1a-5958012935b3', url: 'https://github.com/JohnMops/Central'
+                    } 
+            }
+        stage('some command'){
+            steps{
+            sh 'ls -al && mkdir test && cd test/ && pwd >> text.txt && cat text.txt'
+            }
+                
+            }
+        } 
+    }
