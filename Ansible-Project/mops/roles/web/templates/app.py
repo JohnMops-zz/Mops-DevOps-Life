6f35e81b626d39a723d65340e986c9e45b9d91e9
JohnMops <Evgenibir@outlook.com>
2019-07-16 11:49:22 +0300

    Update app.py

--- Ansible-Project/mops/roles/web/templates/app.py
+++ Ansible-Project/mops/roles/web/templates/app.py
@@ -3,6 +3,6 @@ from flask import Flask, request
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
- return "Testing Super Webhook2 Twice Flask"
+ return "Penis size is good"
 if __name__ == '__main__':
  app.run(host="0.0.0.0", port=80)
