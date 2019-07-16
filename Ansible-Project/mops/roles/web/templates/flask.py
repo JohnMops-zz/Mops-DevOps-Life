a95351c3844a71928386248d9a9e2e38a50d39f4
JohnMops <Evgenibir@outlook.com>
2019-07-16 07:59:08 +0300

    Create flask.py

---
+++ Ansible-Project/mops/roles/web/templates/flask.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+from flask import Flask, request
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def index():
+ return "Testing Flask"
+if __name__ == '__main__':
+ app.run(host="0.0.0.0", port=9006)
