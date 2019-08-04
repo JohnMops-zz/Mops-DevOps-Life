f7a5814e1945961509de473cba190c03e9b175ed
Johnmops <evgenibir@outlook.com>
2019-08-04 11:25:17 +0300

    Worked on flask app

--- Ansible-Project/mops/roles/web/templates/app.py
+++ Ansible-Project/mops/roles/web/templates/app.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 from flask import Flask, request
+
+
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
  return "Nico is the king"
+
+
 if __name__ == '__main__':
  app.run(host="0.0.0.0", port=80)
+
+
