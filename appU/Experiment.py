3f1bc735dbf81924582da54b4dc7bbd8603fb358
Evgeni John Biriukov <Evgenibir@outlook.com>
2019-08-12 13:51:19 +0300

    Update Experiment.py

--- appU/Experiment.py
+++ appU/Experiment.py
@@ -2,6 +2,6 @@ __author__ = 'k0emt'
 
 class Greeter:
     def __init__(self):
-        self.message = 'Hello world!!!'
+        self.message = 'Hello world!!!!'
         # print self.message
 Greeter()
