c5208883f96c644915516c01f8a1a631a6a07091
Evgeni John Biriukov <Evgenibir@outlook.com>
2019-08-14 09:39:57 +0300

    Update ExperimentTests.py

--- appU/ExperimentTests.py
+++ appU/ExperimentTests.py
@@ -4,9 +4,8 @@ import unittest
 from Experiment import Greeter
 
 class MyTestCase(unittest.TestCase):
-    def test_default_greeting_set(self):
-        greeter = Greeter()
-        self.assertEqual(greeter.message, 'Hello world!!!!')
-
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 if __name__ == '__main__':
     unittest.main()
